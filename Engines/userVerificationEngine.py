ae3b105bfbdbe8e404760195071bd2eccbd112ef
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-03-31 21:20:31 -0700

    Created a test file. Contains test data and functions to be tested. Added vars to userVerificationEngine that replace the strings. If the strings need to be changed you can change them in the vars

--- Engines/userVerificationEngine.py
+++ Engines/userVerificationEngine.py
@@ -1,40 +1,76 @@
 #NOTE: Will need to pull from the JSON File and the Database file to verify user login
 #NOTE: Will need to pull from the JSON File and the Database file to verify that a username and email has been used already
 
+
+#imports
 import json
 from random import randint
 import __init__
 from Controllers import DatabaseController, driveDatabaseController, gameDatabaseController
 from Engines import JSONStringEngine
 
+
+#variables that are used in functions. I intend on moving these to a big file
+#   that has all the vars needed so that I can use it inbetween files
+
+#database names
 databaseNameForUsers = "userLoginInfo.db"
+#database layouts
 userLoginInfoTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "UserLoginInfo")
 driveDatabaseTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "Drives")
 gameDatabaseTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "Games")
 
+
+#initial things that are required to be run when the server starts go here
 DatabaseController.database_initialization(databaseNameForUsers, userLoginInfoTBLayout)
 
+#variables that go in the lists and objects go here
+usr            = "Username"
+user           = "user"
+rndID          = "ID"
+login          = "Login"
+acclvl         = "AccessLevel"
+clearenceRoot  = "Root"
+clearenceBasic = "Basic"
+For            = "for"
+forUsr         = "user"
+forGme         = "game"
+forDrv         = "drive"
+funcReq        = "function required"
+err            = "errorcode"
+dsc            = "description"
+eml            = "Email"
+psw            = "Password"
+db             = ".db"
+spt            = "spot"
+at             = "AtValue"
+q1             = "SecurityQ1"
+q2             = "SecurityQ2"
+q3             = "SecurityQ3"
 
 #NOTE: Bug exists where is a username and user's database name may not match
 #       This causes the user to access the wrong drive. Assumed here that the
 #       browser will make sure that the username and the database name match.
-
 #NOTE No encryption will be implemented on the server end. The browser will do
 #       the encryption and send it. The reason being that the bottle framework
 #       doesn't support HTTPS. Because of this, to make sure that the user's data
 #       is as secure as I can make it, the data that the server will recieve will
 #       already be sent, preventing a man in the middle attack. I will hopefully
 #       be transitioning to a flask based server framework for increased security.
-
-
-#NOTE: Before the action can be run, the data required for verification must be checked
-#      The unique identifier must match what is stored in the users_logged_in.json file
-#      and the user must also be logged in as well.
 #NOTE: In the future, IP Addresses may be required to verify that the user is only
 #       accessing their information from one location.
+
+#{
+#   "Username"         :"User_Name",
+#   "ID"               :randint(0, 999999999),
+#   "AccessLevel"      :"basic, root",
+#   "function required":"FileName.FunctionName(*jsonData['args])",
+#   "for"              :"User, drive, game",
+#   "args"             :[arg1, arg2, arg3,...,argN]
+# }
 def verify_user_request(jsonData=dict):
-    selectors = ["Username", "ID", "Login", "AccessLevel"]
-    specifiedUser = DatabaseController.getRows(userLoginInfoTBLayout,{"Username":jsonData["Username"]},databaseNameForUsers, selectors)
+    selectors = [usr, rndID, login, acclvl]
+    specifiedUser = DatabaseController.getRows(userLoginInfoTBLayout,{usr:jsonData[usr]},databaseNameForUsers, selectors)
     errorOnValue = []
     retVal = []
     if type(specifiedUser)==list and len(specifiedUser) == 1:
@@ -46,23 +82,23 @@ def verify_user_request(jsonData=dict):
     else:
         errorOnValue.append(-1)
     if len(errorOnValue) == 0:
-        if jsonData["for"]=="User" and specifiedUser[3] == "Root" and jsonData["for"][:4] =="user":
-            exec(jsonData["function required"])
+        if jsonData[For]==forUsr and specifiedUser[3] == clearenceRoot and jsonData[For][:4] == user:
+            exec(jsonData[funcReq])
         else:
-            exec(jsonData["function required"])
+            exec(jsonData[funcReq])
     else:
         if -1 in errorOnValue:
-            retVal.append({"errorcode":"-1a", "desc":"User not found. Check database."})
+            retVal.append({err:"-1a", dsc:"User not found. Check database."})
         elif 0 in errorOnValue:
-            retVal.append({"errorcode":"-1b", "desc":"Username does not match database"})
+            retVal.append({err:"-1b", dsc:"Username does not match database"})
         elif 1 in errorOnValue:
-            retVal.append({"errorcode":"-1c", "desc":"ID does not match database"})
+            retVal.append({err:"-1c", dsc:"ID does not match database"})
         elif 2 in errorOnValue:
-            retval.append({"errorcode":"-1d", "desc":"User is not logged in, so can not make changes"})
+            retval.append({err:"-1d", dsc:"User is not logged in, so can not make changes"})
         elif 3 in errorOnValue:
-            retVal.append({"errorcode":"-1e", "desc":"User does not have the security clearence to make desired change"})
+            retVal.append({err:"-1e", dsc:"User does not have the security clearence to make desired change"})
         else:
-            retval.append({"errorcode":"-1f", "desc":"An unknown error has occured"})
+            retval.append({err:"-1f", dsc:"An unknown error has occured"})
 #{
 #       "Username":    "username",
 #       "Password":    "password",
@@ -78,23 +114,23 @@ def verify_user_request(jsonData=dict):
 #       "AnswerQ3":    "ANS",
 #   }
 def verify_user_creation(jsonData):
-    searchCriteria = {"Username":jsonData["Username"],"Email":jsonData["Email"]}
+    searchCriteria = {usr:jsonData[usr],eml:jsonData[eml]}
     specifiedUser = DatabaseController.getRowsOr(userLoginInfoTBLayout, searchCriteria, databaseNameForUsers, list(searchCriteria.keys()))
     retVal = []
     if type(specifiedUser) == list and len(specifiedUser) == 0:
             DatabaseController.addrow(userLoginInfoTBLayout, list(jsonData.values()),databaseNameForUsers)
-            DatabaseController.database_initialization(searchCriteria["Username"]+".db", driveDatabaseTBLayout)
-            DatabaseController.database_initialization(searchCriteria["Username"]+".db", gameDatabaseTBLayout)
+            DatabaseController.database_initialization(searchCriteria[usr]+db, driveDatabaseTBLayout)
+            DatabaseController.database_initialization(searchCriteria[usr]+db, gameDatabaseTBLayout)
     else:
         for item in specifiedUser:
-            if item[0] == jsonData["Username"] and item[1] == jsonData["Email"]:
-                retVal.append({"Errorcode":"-6ab","desc":"User already exists with that username and email. Try again"})
-            elif item[0] == jsonData["Username"] and item[1] != jsonData["Email"]:
-                retVal.append({"Errorcode":"-6a","desc":"User already exists with that username. Try again"})
-            elif item[0] != jsonData["Username"] and item[1] == jsonData["Email"]:
-                    retVal.append({"Errorcode":"-6b","desc":"User already exists with that email. Try again"})
+            if item[0] == jsonData[usr] and item[1] == jsonData[eml]:
+                retVal.append({err:"-6ab",dsc:"User already exists with that username and email. Try again"})
+            elif item[0] == jsonData[usr] and item[1] != jsonData[eml]:
+                retVal.append({err:"-6a",dsc:"User already exists with that username. Try again"})
+            elif item[0] != jsonData[usr] and item[1] == jsonData[eml]:
+                    retVal.append({err:"-6b",dsc:"User already exists with that email. Try again"})
             else:
-                retVal.append({"Errorcode":"-6c","desc":"unknown"})
+                retVal.append({err:"-6c",dsc:"unknown"})
         return retVal
     
 
@@ -112,18 +148,18 @@ def verify_user_creation(jsonData):
 #   Password:password
 # }
 def verify_user_login(jsonData):
-    selectors = ["Username", "Password"]
-    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, {"Username": jsonData["Username"], "Password": jsonData["Password"]}, databaseNameForUsers, selectors)
+    selectors = [usr, psw]
+    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, {usr: jsonData[usr], psw: jsonData[psw]}, databaseNameForUsers, selectors)
     if type(matchingUser) ==list and len(matchingUser)==1:
             #The way that this is set up allows you to log in from another browser. In theory,
             #you could log in through anouther browser and reset the ID this way. I am not sure
             #if I want to keep it this way though. I only want one browser at a time to access
             #this data
-            DatabaseController.modifyRow(databaseNameForUsers, userLoginInfoTBLayout, {"ID":randint(0, 999999999), "Login":1}, {"spot":"Username", "AtValue":jsonData["Username"]})
+            DatabaseController.modifyRow(databaseNameForUsers, userLoginInfoTBLayout, {rndID:randint(0, 999999999), "Login":1}, {spt:usr, at:jsonData[usr]})
     elif len(matchingUser) < 1: 
-        return {"errorcode":"7a", "desc":"No user found with the given information. Try again"}
+        return {err:"7a", dsc:"No user found with the given information. Try again"}
     else:
-        return {"errorcode":"7b", "desc":"Database error. Contact customer support"}
+        return {err:"7b", dsc:"Database error. Contact customer support"}
 
 #{
 #   Username:user_name,
@@ -131,28 +167,27 @@ def verify_user_login(jsonData):
 #   Login:1
 # }
 def verify_user_logout(jsonData):
-    searchCriteria = {"Username":jsonData["Username"], "ID":jsonData["ID"], "Login":jsonData["Login"]}
-    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, searchCriteria, databaseNameForUsers)
+    searchCriteria = {usr:jsonData[usr], rndID:jsonData[rndID], login:jsonData[login]}
+    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, searchCriteria, databaseNameForUsers, [usr, rndID, login])
     if type(matchingUser) == list:
         if len(matchingUser)==1:
-            DatabaseController.modifyRow(databaseNameForUsers, userLoginInfoTBLayout, {"ID":0, "Login":0}, {"spot":"Username", "AtValue":jsonData["Username"]})
+            DatabaseController.modifyRow(databaseNameForUsers, userLoginInfoTBLayout, {rndID:0, login:0}, {spt:usr, at:jsonData[usr]})
         elif len(matchingUser) > 1:
-            return {"errorcode":"8a","desc":"More than one user with the given data"}
+            return {err:"8a",dsc:"More than one user with the given data"}
         else:
-            return {"errorcode":"8b", "desc":"No users found with the given data"}
+            return {err:"8b", dsc:"No users found with the given data"}
     else:
-        return {"errorcode":"8c", "desc":"Database does not exist. Contact customer support."}
+        return {err:"8c", dsc:"Database does not exist. Contact customer support."}
 
 #{
 #   Username:user_name
 #   Email: email
 # }
 def verify_password_reset(jsonData):
-    searchCriteria = {"Username":jsonData["Username"], "Email": jsonData["Email"]}
-    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, searchCriteria, databaseNameForUsers)
-    if type(matchingUser) == list:
-        if length(matchingUser) == 1:
-            return None
+    searchCriteria = {usr:jsonData[usr], eml: jsonData[eml]}
+    matchingUser = DatabaseController.getRows(userLoginInfoTBLayout, searchCriteria, databaseNameForUsers, [q1, q2, q3])
+    if type(matchingUser) == list and len(matchingUser) == 1:
+        return list[matchingUser[0]]
     return None
 
 
@@ -161,13 +196,13 @@ def initiate_password_reset():
 
 #DatabaseController.addrow(jsonDataRetrieved, ["goldeneye5671", "Password", "choice@choice.com", 0, 1, "Root", "", "", "", "", "", ""], databaseNameForUsers)
 #print(verify_user_request(sentData1))
-for item in test:
-    print(verify_user_creation(item))
+# for item in test:
+#     print(verify_user_creation(item))
 
-verify_user_login({"Username":"Fantasy89", "Password":"Password"})
+# verify_user_login({usr:"Fantasy89", psw:psw})
 
-for item in commands:
-    print (verify_user_request(item))
+# for item in commands:
+#     print (verify_user_request(item))
 
 #eval(sentAct["function required"])
 
--- Test Files/testData.py
+++ Test Files/testData.py
@@ -1,3 +1,42 @@
+import __init__
+from Engines import userVerificationEngine, JSONStringEngine
+
+#database names
+databaseNameForUsers = "userLoginInfo.db"
+#database layouts
+userLoginInfoTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "UserLoginInfo")
+driveDatabaseTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "Drives")
+gameDatabaseTBLayout = JSONStringEngine.retrieve_all_database_entries("database_info.json", "./JSON Data/", "Games")
+
+
+#initial things that are required to be run when the server starts go here
+#DatabaseController.database_initialization(databaseNameForUsers, userLoginInfoTBLayout)
+
+#variables that go in the lists and objects go here
+usr            = "Username"
+user           = "user"
+rndID          = "ID"
+login          = "Login"
+acclvl         = "AccessLevel"
+clearenceRoot  = "Root"
+clearenceBasic = "Basic"
+For            = "for"
+forUsr         = "user"
+forGme         = "game"
+forDrv         = "drive"
+funcReq        = "function required"
+err            = "errorcode"
+dsc            = "description"
+eml            = "Email"
+psw            = "Password"
+db             = ".db"
+spt            = "spot"
+at             = "AtValue"
+q1             = "SecurityQ1"
+q2             = "SecurityQ2"
+q3             = "SecurityQ3"
+
+
 driveInfo =[
     {
         "DriveName":"d1",
@@ -164,3 +203,431 @@ commands = [
         "args":["Fantasy89.db", driveDatabaseTBLayout, driveInfo[8]]
     }
 ]
+
+
+users = [
+    {
+        "Username":"Fantasy89",
+        "Password":"abc123",
+        "Email":"test1@test.com",
+        "ID":"1",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Color",
+        "AnswerQ1":"Purple",
+        "SecurityQ2":"Hot",
+        "AnswerQ2":"YEA",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Tdeves1",
+        "Password":"abc123",
+        "Email":"test2@test.com",
+        "ID":"2",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Fruit",
+        "AnswerQ1":"Apple",
+        "SecurityQ2":"Cold",
+        "AnswerQ2":"Nah",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"goldeneye5671",
+        "Password":"abc123",
+        "Email":"test3@test.com",
+        "ID":"3",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Veggie",
+        "AnswerQ1":"Tomato",
+        "SecurityQ2":"Music",
+        "AnswerQ2":"Piano",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"geye5671",
+        "Password":"abc123",
+        "Email":"test4@test.com",
+        "ID":"4",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Pet",
+        "AnswerQ1":"Rabbit",
+        "SecurityQ2":"Food",
+        "AnswerQ2":"Meatballs",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Bold",
+        "Password":"abc123",
+        "Email":"test5@test.com",
+        "ID":"5",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Music",
+        "AnswerQ1":"Alternative",
+        "SecurityQ2":"Friend",
+        "AnswerQ2":"I love the homies",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TonyMAC",
+        "Password":"abc123",
+        "Email":"test6@test.com",
+        "ID":"6",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Book",
+        "AnswerQ1":"The Hungry Games",
+        "SecurityQ2":"Console",
+        "AnswerQ2":"Amiga",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TonyPC",
+        "Password":"abc123",
+        "Email":"test7@test.com",
+        "ID":"7",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Hyper",
+        "AnswerQ1":"Nah dude",
+        "SecurityQ2":"Pet",
+        "AnswerQ2":"Dog",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TonyiOS",
+        "Password":"abc123",
+        "Email":"test8@test.com",
+        "ID":"8",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Console",
+        "AnswerQ1":"Switch",
+        "SecurityQ2":"Android",
+        "AnswerQ2":"Hell na",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TonyAndroid",
+        "Password":"abc123",
+        "Email":"test9@test.com",
+        "ID":"9",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Color",
+        "AnswerQ1":"Blue",
+        "SecurityQ2":"State",
+        "AnswerQ2":"NY",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TimApple",
+        "Password":"abc123",
+        "Email":"test10@test.com",
+        "ID":"10",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Fruit",
+        "AnswerQ1":"Apple",
+        "SecurityQ2":"Game",
+        "AnswerQ2":"Bioshok: Infaninte",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"TheDankestPods",
+        "Password":"abc123",
+        "Email":"test11@test.com",
+        "ID":"11",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Veggie",
+        "AnswerQ1":"Artichoke",
+        "SecurityQ2":"Streamer",
+        "AnswerQ2":"Nope",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"FrozenBuds",
+        "Password":"abc123",
+        "Email":"test12@test.com",
+        "ID":"12",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"iOS",
+        "AnswerQ1":"NO",
+        "SecurityQ2":"Social",
+        "AnswerQ2":"Twitter",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"AvitarMaster",
+        "Password":"abc123",
+        "Email":"test13@test.com",
+        "ID":"13",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Android",
+        "AnswerQ1":"...maybe",
+        "SecurityQ2":"Veggie",
+        "AnswerQ2":"Cucumber",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"dank_meme_god",
+        "Password":"abc123",
+        "Email":"test14@test.com",
+        "ID":"14",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Music",
+        "AnswerQ1":"Pop",
+        "SecurityQ2":"Fruit",
+        "AnswerQ2":"Tomato",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"CommandMan",
+        "Password":"abc123",
+        "Email":"test15@test.com",
+        "ID":"15",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Color",
+        "AnswerQ1":"Green",
+        "SecurityQ2":"Friend",
+        "AnswerQ2":"Meh...maybe...",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"OneNastyBoi",
+        "Password":"abc123",
+        "Email":"test16@test.com",
+        "ID":"16",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Fruit",
+        "AnswerQ1":"Moldy Orange",
+        "SecurityQ2":"state",
+        "AnswerQ2":"CA",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"MadGuy",
+        "Password":"abc123",
+        "Email":"test17@test.com",
+        "ID":"17",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Music",
+        "AnswerQ1":"Clasic Rock",
+        "SecurityQ2":"Color",
+        "AnswerQ2":"teal",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"SadGuy",
+        "Password":"abc123",
+        "Email":"test18@test.com",
+        "ID":"18",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Book",
+        "AnswerQ1":"Im catching fire now!",
+        "SecurityQ2":"Cat",
+        "AnswerQ2":"Allergic",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"ShyGuy",
+        "Password":"abc123",
+        "Email":"test19@test.com",
+        "ID":"19",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Hobby",
+        "AnswerQ1":"Singing",
+        "SecurityQ2":"Car",
+        "AnswerQ2":"Audi",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"MarioMario",
+        "Password":"abc123",
+        "Email":"test120@test.com",
+        "ID":"120",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Pet",
+        "AnswerQ1":"Mouse",
+        "SecurityQ2":"Hobby",
+        "AnswerQ2":"Racing",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Luigi Mario",
+        "Password":"abc123",
+        "Email":"test121@test.com",
+        "ID":"121",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"PC",
+        "AnswerQ1":"Nah",
+        "SecurityQ2":"Hobby",
+        "AnswerQ2":"Dancing",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"H20Delerious",
+        "Password":"abc123",
+        "Email":"test122@test.com",
+        "ID":"122",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Apple",
+        "AnswerQ1":"Yeah bro",
+        "SecurityQ2":"Hobby",
+        "AnswerQ2":"Writting Books",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"VannossGaming",
+        "Password":"abc123",
+        "Email":"test124@test.com",
+        "ID":"123",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Friend",
+        "AnswerQ1":"I have none",
+        "SecurityQ2":"PC",
+        "AnswerQ2":"base",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"iwantausername",
+        "Password":"abc123",
+        "Email":"test125@test.com",
+        "ID":"124",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Color",
+        "AnswerQ1":"Brown",
+        "SecurityQ2":"PC",
+        "AnswerQ2":"laptop",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"ohforcryingoutloudgivemeausername",
+        "Password":"abc123",
+        "Email":"test131@test.com",
+        "ID":"125",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Veggie",
+        "AnswerQ1":"Lettuce",
+        "SecurityQ2":"Apple",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Zeldom",
+        "Password":"abc123",
+        "Email":"test130@test.com",
+        "ID":"126",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Shinny",
+        "AnswerQ1":"Swoard",
+        "SecurityQ2":"Apple",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"gameranks",
+        "Password":"abc123",
+        "Email":"test126@test.com",
+        "ID":"127",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Car",
+        "AnswerQ1":"VW",
+        "SecurityQ2":"Apple",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Stonks",
+        "Password":"abc123",
+        "Email":"test127@test.com",
+        "ID":"128",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Music",
+        "AnswerQ1":"Jazz",
+        "SecurityQ2":"Car",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"Optimus",
+        "Password":"abc123",
+        "Email":"test128@test.com",
+        "ID":"129",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Warm",
+        "AnswerQ1":"NO!",
+        "SecurityQ2":"Car",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+    {
+        "Username":"OptimusPrime",
+        "Password":"abc123",
+        "Email":"test129@test.com",
+        "ID":"1230",
+        "Login":"0",
+        "AccessLevel":"Basic",
+        "SecurityQ1":"Cold",
+        "AnswerQ1":"maybe...",
+        "SecurityQ2":"Car",
+        "AnswerQ2":"No",
+        "SecurityQ3":"",
+        "AnswerQ3":""
+    },
+]
+
+
+for item in users:
+    print(userVerificationEngine.verify_user_creation(item))
