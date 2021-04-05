6599cd8556e7b7d7e98858408572e0b91a8e3102
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2021-04-05 14:51:14 -0700

    Added functions that do conversions to make other functions less complex. Also created some functions to reduce the complexity of other functions. Please refer to changes to see.

--- Controllers/gameDatabaseController.py
+++ Controllers/gameDatabaseController.py
@@ -23,53 +23,63 @@ gameDatabaseTBLayout = JSONStringEngine.retrieve_all_database_entries("database_
 #   size_of_game: 485.67
 #   size_metric: gb
 # }
-def is_enough_space(usr, drv, gameSize, gameMetric):
-    driveSizeMetrics = []
-    driveSize = DatabaseController.getRows(driveDatabaseTBLayout, {"DriveName":drv}, usr+".db", ["RemainingSpaceOnDrive", "DriveSizeMetric"])
-    if type(driveSize) == list and len(driveSize) == 1:
-        driveSize = list(driveSize[0])
-        match gameMetric:
-            case "mb":
-                driveSizeMetrics.append(1)
-            case "gb":
-                driveSizeMetrics.append(2)
-            case "tb":
-                driveSizeMetrics.append(3)
-            case _:
-                driveSizeMetric.append(-1)
-        match driveSize[1]:
-            case "mb":
-                driveSizeMetrics.append(1)
-            case "gb":
-                driveSizeMetrics.append(2)
-            case "tb":
-                driveSizeMetrics.append(3)
-            case _:
-                driveSizeMetric.append(-1)
-        #givenToMe              database
-    conversionVal = None
-    
-    if driveSizeMetrics[0] < driveSizeMetrics[1]:
-        conversionVal = gameSize
-        if driveSizeMetrics[1]-driveSizeMetrics[0] > 1:
-        #to convert, either devide by or multiply by 1,000
-            for i in range(driveSizeMetrics[1]-driveSizeMetrics[0]):
-                conversionVal/=1000
-                conversionVal = float(driveSize[0])-conversionVal
-            return [conversionVal > 0, conversionVal]
+
+def makeSizeMetricInt(value):
+    match value:
+        case "mb":
+            return 1
+        case "gb":
+            return 2
+        case "tb":
+            return 3
+        case _:
+            return -1
+
+
+def makeSizeMetricStr(value):
+    match value:
+        case 1:
+            return "mb"
+        case 2:
+            return "gb"
+        case 3:
+            return "tb"
+        case _:
+            return "unknown"
+
+#[
+#   100,
+#   "gb"
+# ]
+#converts the smaller size to the bigger size, if there is a smaller
+#and bigger size metric
+def convert_to_Larger_Value(value1=[], value2=[]):
+    value1[1] = makeSizeMetricInt(value1[1])
+    value2[1] = makeSizeMetricInt(value2[1])
+    if float(value1[1]) > float(value2[1]):
+        for i in range(value1[1]-value2[1]):
+            value2[0] /= 1000
+        value2[1] = value1[1]
+    elif float(value1[1]) < float(value2[1]):
+        for i in range(value2[1]-value1[1]):
+            value1[0]/=1000
+        value1[1] = value2[1]
+    return [value1, value2]
+
+
+def is_enough_space(usr, drv, gmeInfo):
+    drvInfo = DatabaseController.getRows(driveDatabaseTBLayout, {"DriveName":drv}, usr+".db", ["RemainingSpaceOnDrive", "DriveSizeMetric"])
+    if type(drvInfo) == list and len(drvInfo) == 1:
+        drvInfo = list(drvInfo[0])
+        sameSize = convert_to_Larger_Value([gmeInfo[0], gmeInfo[1]], [float(drvInfo[0]), drvInfo[1]])
+        drvInfo = sameSize[1]
+        gmeInfo = sameSize[0]
+        if gmeInfo[0] > drvInfo[0]:
+            return [False, drvInfo[0]-gmeInfo[0]]
         else:
-            conversionVal/=1000
-            conversionVal = float(driveSize[0]) - conversionVal
-            return [conversionVal > 0, conversionVal]
-    #if the game has a larger size metric that would mean that the game is bigger than the drive
-    elif driveSizeMetrics[0] > driveSizeMetrics[1]:
-        return [False, -1]
-    else:
-        if gameSize > float(driveSize[0]):
-            return [False, -1]
-        else:
-            return [float(driveSize[0])-gameSize > 0, conversionVal]
-#print(is_enough_space("Fantasy89", "d1", 900, "gb"))
+            return [True, drvInfo[0]-gmeInfo[0]]
+
+#print(is_enough_space("Fantasy89", "d1", [900, "gb"]))
 
 
 #opens game drive database
@@ -78,12 +88,12 @@ def is_enough_space(usr, drv, gameSize, gameMetric):
 #inserts a game entry
 #saves the modification
 #closes the database
-def addGame(usr, drv, name, size, metric, tags, added, time):
-    space = is_enough_space(usr, drv, size, metric)
+def addGame(usr, drv, gameName, gameSize, gameMetric, gameTags, gameAdded, gameTime):
+    space = is_enough_space(usr, drv, [gameSize, gameMetric])
     if space[0]:
-        gamesOnDriveByName = databaseCommandEngine.retrieve_row(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, {"GameName":name}, ["GameName"])
+        gamesOnDriveByName = databaseCommandEngine.retrieve_row(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, {"GameName":gameName}, ["GameName"])
         if type(gamesOnDriveByName) == list and len(gamesOnDriveByName) == 0:
-            gameToAdd = {"GameName":name, "Size": size, "GameSizeMetric":metric, "GameTags":tags, "DateAdded":added, "PlayTime":time}
+            gameToAdd = {"GameName":gameName, "Size": gameSize, "GameSizeMetric":gameMetric, "GameTags":gameTags, "DateAdded":gameAdded, "PlayTime":gameTime}
             databaseCommandEngine.insert_row(usr+".db",gameDatabaseTBLayout["Database Directory"], drv, gameDatabaseTBLayout["Database Headers"], list(gameToAdd.values()))
             # DatabaseController.modifyRow(usr+".db", driveDatabaseTBLayout, {"RemainingSpaceOnDrive":space[1]},)
             driveDatabaseController.modify_drive(usr+".db", driveDatabaseTBLayout, {"RemainingSpaceOnDrive":space[1]}, {"spot":"DriveName", "AtValue":drv}, ["*"])
@@ -93,7 +103,13 @@ def addGame(usr, drv, name, size, metric, tags, added, time):
         return {"errorcode":"10", "desc":"Not enough space remaining"}
 
 addGame("Fantasy89", "d1", "Bioshock: Infinate", 192, "gb", "", "4-3-2021", 200)
-
+addGame("Fantasy89", "d1", "Crackdown 3", 204, "gb", "", "4-3-2021", 200)
+addGame("Fantasy89", "d1", "Anthem", 125, "gb", "", "4-3-2021", 200)
+addGame("Fantasy89", "d1", "Star Wars: Battlefront", 230, "gb", "", "4-3-2021", 200)
+addGame("Fantasy89", "d1", "Tom Clancy's The Division 2", 120, "gb", "", "4-3-2021", 200)
+addGame("Fantasy89", "d1", "Sonic Mania Plus", 900, "mb", "", "4-3-2021", 200)
+#871.9 gb
+#.8719 tb
 
 #opens the game drive database
 #opens table that contains the correct drive name
@@ -115,62 +131,43 @@ def retrieve_all_games_on_drive(usr, drv):
     return databaseCommandEngine.retrieve_table(usr+".db", gameDatabaseTBLayout["Database Directory"], drv)
 
 
-#opens the game drive database
-#opens table that contains the correct drive name
-#uses a select statement to select the game entry to modify or else makes no modification if game doesn't exist
-#saves the retrieved data to a variable
-#deletes the old entry
-#modifies the retrieved entry
-#inserts the modified entry to the table with the correct drive name
-#saves the modification
-#closes the database
-
-#Expected layout for adding more data to a game or subtracting data
-#{
-#   Size:20,
-#   GameSizeMetric:gb
-# }
-#Expected layout for everythine else shouldn't matter as much
-#{
-#   GameName: newGameName,
-#   GameTags: newGameTags,
-#   DateAdded: 18-04-2021
-#   Playtime: newplaytime
-# }
-#Can't change the size metric because that would require conversion
-#To change the size metric, the user will have to delete the game and
-#add the game again using the size metric they want
-def modify_game_on_drive(usr, drv, name, updatedValues_Dict, spot, action):
-    matchingGames = retrieve_game_on_drive(usr, drv, name)
-    if type(matchingGames) == list and len(matchingGames) == 1:
-        matchingGames = list(matchingGames[0])
-        match action:
-            case "+":
-                space = is_enough_space(usr, drv, updatedValues_Dict["Size"], updatedValues_Dict["GameSizeMetric"])
-                if space[0] and matchingGames[2] == updatedValues_Dict["GameSizeMetric"]:
-                    updatedValues_Dict["Size"] = matchingGames[1] + updatedValues_Dict["Size"]
-                    databaseCommandEngine.update_table_at_spot(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, updatedValues_Dict, {"spot":"GameName", "AtValue": name})
-                    databaseCommandEngine.update_table_at_spot(usr+".db", driveDatabaseTBLayout["Database Directory"],driveDatabaseTBLayout["Database Tables"][0], {"RemainingSpaceOnDrive":space[1]}, {"spot":"DriveName", "AtValue":drv})
-                    # driveDatabaseController.modify_drive()
-                    return space[1]
-            case "-":
-                if updatedValues_Dict["GameSizeMetric"] ==matchingGames[2]:
-                    updatedValues_Dict["Size"] = matchingGames[1] + updatedValues_Dict["Size"]
-                    databaseCommandEngine.update_table_at_spot(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, updatedValues_Dict, {"spot": "GameName", "AtValue":name})
-                    driveSizeMetric = driveDatabaseController.retrieve_drives(usr+".db", driveDatabaseTBLayout, {"DriveName": drv}, ["DriveSizeMetric"])[0]
-                    if driveSizeMetric[0] == matchingGames[2]:
-
-                    elif driveSizeMetric == "tb" and matchingGames[2] == "gb":
-
-                    else:
-                        
-                return None
-            case _:
-                return None
+#adds an update to a game if there is enough space remaining on a drive
+def add_update_to_game(usr, drv, gameName, gameSize, gameMetric):
+    space = is_enough_space(usr, drv, [gameSize, gameMetric])
+    if space[0]:
+        gamesOnDriveByName = databaseCommandEngine.retrieve_row(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, {"GameName":gameName}, ["GameName", "Size", "GameSizeMetric"])
+        if type(gamesOnDriveByName) == list and len(gamesOnDriveByName) == 1:
+            gamesOnDriveByName = list(gamesOnDriveByName[0])
+            gameToUpdate = {}
+            if gamesOnDriveByName[2] == gameMetric:
+                gameToUpdate = {"GameName":gameName, "Size": gameSize + gamesOnDriveByName[1]}
+            else:
+                convertToLarger = convert_to_Larger_Value([gameSize, gameMetric],[gamesOnDriveByName[1], gamesOnDriveByName[2]])
+                convertToLarger[0][1] = makeSizeMetricStr(convertToLarger[0][1])
+                gameToUpdate = {"GameName":gameName, "Size": convertToLarger[0][0]+convertToLarger[1][0], "GameSizeMetric": convertToLarger[0][1]}
+            driveToUpdate = {"RemainingSpaceOnDrive": space[1]}
+            databaseCommandEngine.update_table_at_spot(usr+".db", gameDatabaseTBLayout["Database Directory"], drv, gameToUpdate, {"spot":"GameName", "AtValue": gameName})
+            databaseCommandEngine.update_table_at_spot(usr+".db", driveDatabaseTBLayout["Database Directory"], driveDatabaseTBLayout["Database Tables"][0], driveToUpdate, {"spot":"DriveName","AtValue":drv})
+        else:
+            return {"errorcode":"12","desc":"game doesn't exist"}
     else:
-        return {"errorcode":"12", "desc":"Game does not exist"}
+        return {"errorcode":"13", "desc":"Not enough space"}
 
-modify_game_on_drive("Fantasy89", "d1", "Bioshock: Infinate", {"Size":20, "GameSizeMetric":"gb"}, {"spot":"GameName", "AtValue":"Bioshock: Infinate"}, "-")
+print(add_update_to_game("Fantasy89", "d1", "Bioshock: Infinate", 1, "tb"))
+print(add_update_to_game("Fantasy89", "d1", "Bioshock: Infinate", 1, "tb"))
+print(add_update_to_game("Fantasy89", "d1", "Bioshock: Infinate", 1, "tb"))
+print(add_update_to_game("Fantasy89", "d1", "Bioshock: Infinate", 1, "tb"))
+print(add_update_to_game("Fantasy89", "d1", "Bioshock: Infinate", 1, "tb"))
+
+#removes an update/content from a game
+def remove_content_from_game():
+    return None
+
+#Changes the name, playtime, add-date, etc of a game. Will not
+#change the storage inforamtion
+def update_other_from_game():
+    return None
+
 
 #NOTE: Will need drive name so that it deletes the game from the right drive
 #NOTE: Will need game name so that it deletes the game with the correct name
